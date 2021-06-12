fe821cc889e6054d22abd323fcc2d169e250da9f
Taromaru <48397556+Taromaruu@users.noreply.github.com>
2021-06-12 09:29:46 -0400

    feat(os): Made Pattern and added basic windows

---
+++ js/desktop.js
@@ -0,0 +1,11 @@
+const window_blue = "#0000A8";
+
+new WinBox("Program Manager", {
+    class: "program-manager"
+});
+
+new WinBox("Window", {
+    x: "center",
+    y: "center",
+    html: "<p class='vgasys'>Hello, world!</p>"
+})
